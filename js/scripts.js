9d05234b8cc88021ef134bfba2f135cd4b82eb3e
Caleb Stokka and Cameron Poore <caleb+cameron@>
2016-06-15 09:54:28 -0700

    reset temp score when standing, alert when game over

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,25 @@
 //backend logic
+var tempScore = 0;
+var totalScore = 0;
+
 function roll() {
   result = Math.floor((Math.random() * 6) + 1);
-  return result;
+  if (result === 1) {
+    tempScore *= 0;
+    return 0;
+  }
+  else {
+    return result;
+  }
 }
-var tempScore = 0;
 function runningScore(rollResult) {
   tempScore += rollResult;
   return tempScore;
 }
-totalScore = 0;
-function TotalScore(tempScore){
+
+function stand(tempScore){
   totalScore += tempScore;
+  tempScore *= 0;
   return totalScore;
 }
 
@@ -19,17 +28,19 @@ $(document).ready(function() {
 
   $("#roll").click(function() {
     var rollResult = roll();
-    console.log(rollResult);
     $("#die h2").text(rollResult);
     $("#tempScore h2").text(runningScore(rollResult));
   });
+
   $("#stand").click(function(){
-    if (totalScore >=100){
+    $("#totalScore h2").text(stand(tempScore));
+    tempScore *= 0;
+    $("#tempScore h2").text(tempScore);
+    if (totalScore >=100) {
       return alert("GAME OVER");
-      totalScore = 0;
-      tempScore = 0;
-    } else {
-      $("#totalScore h2").text(TotalScore(tempScore));
+      totalScore *= 0;
     }
+    console.log(tempScore);
   });
+
 });

17ed270490e3516954d299db1ec17194f03e89e6
Caleb Stokka and Cameron Poore <caleb+cameron@>
2016-06-16 13:34:44 -0700

    added two dice

--- js/objectdice.js
+++ js/objectdice.js
@@ -25,13 +25,21 @@ var player2 = new Player(-1,0,0);
 var game = new Game(1, false, 0, 0, [player1, player2]);
 
 Player.prototype.roll = function () {
-  rollResult = Math.floor((Math.random() * 6) + 1);
+  die1 = Math.floor((Math.random() * 6) + 1);
+  die2 = Math.floor((Math.random() * 6) + 1);
+  rollResult = die1 + die2
   this.tempScore += rollResult;
-  if (rollResult === 1) {
+  console.log(die1);
+  console.log(die2);
+  if (die1 === 1 && die2 === 1) {
+    this.tempScore = 0;
+    this.score = 0;
+    game.switchTurn();
+  }
+  else if (die1 === 1 || die2 === 1) {
     this.tempScore = 0;
     game.switchTurn();
   }
-  console.log(rollResult);
 }
 
 Game.prototype.robotTurn = function () {
@@ -50,18 +58,18 @@ Game.prototype.switchTurn = function () {
 }
 
 Player.prototype.stand = function () {
-  game.switchTurn();
   this.score += this.tempScore;
   this.tempScore = 0;
   game.playerOneScore = player1.score;
   game.playerTwoScore = player2.score;
-  if (game.playerOneScore >=10 || game.playerTwoScore >= 10) {
+  if (game.playerOneScore >=20 || game.playerTwoScore >= 20) {
     game.gameOver = true;
     alert("GAME OVER - Player one score: " + game.playerOneScore + " Player two score: " + game.playerTwoScore);
   game = new Game(1, false, 0, 0, [player1, player2]);
   player1 = new Player(1,0,0);
   player2 = new Player(-1,0,0);
   }
+  game.switchTurn();
   $("#playerOneScore h2").text(player1.score);
   $("#playerTwoScore h2").text(player2.score);
   $("#tempScore h2").text("");
@@ -72,7 +80,7 @@ $(document).ready(function() {
   $("#roll").click(function() {
     player1.roll();
     $("#tempScore h2").text(player1.tempScore);
-    $("#die p").text(dice[rollResult]);
+    $("#die p").text(dice[die1] + dice[die2]);
   });
 
   $("#stand").click(function(){
